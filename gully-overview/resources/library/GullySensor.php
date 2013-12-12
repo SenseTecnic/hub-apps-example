f8d6036b8c0e82270dd6ca01ac65561ed50b1bfd
Crystal Ng <crystalng.w@gmail.com>
2013-12-12 13:35:34 -0800

    changd the population script

--- gully-overview/resources/library/GullySensor.php
+++ gully-overview/resources/library/GullySensor.php
@@ -9,16 +9,16 @@
 class GullySensor {
 
 	//class variables
-	private $sensorid=null;
-	private $gullyid=null;
+	private $sid=null;
+	private $gid=null;
 	private $timestamp=null;
-	private $sensorname=null;
-	private $gullytype=null;
-	private $gullystate=null;
-	private $lat= null;
-	private $lng= null;
-	private $siltlevel= null;
-	private $accessibility = null;
+	private $name=null;
+	private $ty=null;
+	private $st=null;
+	private $la= null;
+	private $ln= null;
+	private $si= null;
+	private $access = null;
 	private $lastupdate=null;
 
   	public function __construct($data, $lastupdate){
@@ -28,45 +28,43 @@ class GullySensor {
   		foreach($decoded_object["data"][0] as $key=>$value){
         $key = strtolower($key);
 		  if(strpos($key, "silt")!==false){
-		    $this->siltlevel= $value;
+		    $this->si= $value;
 		  }
 		  if(strpos($key, "state")!==false){//state
 		  	if (strtolower($value) =="clean and running")
-                $this->gullystate= "Clean & Running";
+                $this->st= "Clean & Running";
             else if (strtolower($value)=="blocked and cleaned")
-                $this->gullystate= "Blocked & Cleaned";
+                $this->st= "Blocked & Cleaned";
             else if (strtolower($value)=="cleaned and not running")
-                $this->gullystate= "Cleaned & Not Running";
+                $this->st= "Cleaned & Not Running";
             else if (strtolower($value)=="obstructed")
-                $this->gullystate= "Obstructed";
+                $this->st= "Obstructed";
             else
-                $this->gullystate= "No Info";
-		    $this->gullystate= $value;
+                $this->st= "No Info";
+		    $this->st= $value;
 		  }
 		  if(strpos($key, "type")!==false){//type
-		    $this->gullytype= $value;
+		    $this->ty= $value;
 		  }
 		  if(strpos($key, "accessibility")!==false){
-		    $this->accessibility= $value;
+		    $this->access= $value;
 		  }
 		}
-  		$this->gullyid= $decoded_object["data"][0]["id"];
-  		$this->sensorid= $decoded_object["data"][0]["sensor_id"];
+  		$this->gid= $decoded_object["data"][0]["id"];
+  		$this->sid= $decoded_object["data"][0]["sensor_id"];
   		$this->timestamp= $decoded_object["data"][0]["timestamp"];
-  		$this->sensorname= $decoded_object["data"][0]["sensor_name"];
-  		$this->lat= $decoded_object["data"][0]["lat"];
-  		$this->lng= $decoded_object["data"][0]["lng"];
+  		$this->name= $decoded_object["data"][0]["sensor_name"];
+  		$this->la= $decoded_object["data"][0]["lat"];
+  		$this->ln= $decoded_object["data"][0]["lng"];
   	}
 
   	public function create_DB_object(){
   		$gully= array();
   		foreach (get_object_vars($this) as $key=>$value){
-  			if ($key != "lat" && $key != "lng"){
   				$gully[$key]=$value;
-  			}
   		}
   		$geojson["type"]="Point";
-        $geojson["coordinates"]=array((float)$this->lng, (float)$this->lat);
+        $geojson["coordinates"]=array((float)$this->ln, (float)$this->la);
         $gully["geo"]= $geojson;
   		return $gully;
   	}
